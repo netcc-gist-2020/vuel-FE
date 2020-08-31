1f0a0244c57c4bb8a41eeafe92643ff5b5b30066
Jonghun Park <jonghun.park.194@gmail.com>
2020-08-31 17:33:02 +0900

    Add a basic MyPage frame

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -1,17 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>This is mypage</h1>
-    <v-btn>
-      <router-link to="classroom">join</router-link>
-    </v-btn>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <h1>Changa Lee</h1>
+        <v-row>
+          <v-btn text>Profile</v-btn>
+        </v-row>
+        <v-row>
+          <v-btn text>Class</v-btn>
+        </v-row>
+        <v-row>
+          <v-btn text>Avatar</v-btn>
+        </v-row>
+      </v-col>
+
+      <v-col cols="4">
+        <v-row></v-row>
+        <v-row>
+          <v-form>
+            <p>Classroom ID</p>
+            <v-text-field placeholder="Enter" filled>
+            </v-text-field>
+            <v-btn
+              color="success"
+            >
+              <router-link to="/classroom">
+                JOIN
+              </router-link>
+            </v-btn>
+          </v-form>
+        </v-row>
+      </v-col>
+
+      <v-col cols="4">
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'Home',
+  name: 'MyPage',
   components: {
   }
 }
