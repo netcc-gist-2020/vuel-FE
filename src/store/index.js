08ecedd2ff67fb92a3a8b168ba0f737f7f6ab1a7
kimjngyun <kimjngyun@kimjngyunui-MacBookPro.local>
2020-09-05 16:38:46 +0900

    rm init guests

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ export default new Vuex.Store({
     amIHost: true,
     classRoomID: '',
     myID: '',
-    guestList: { A: 'happy', B: 'neutral' }
+    guestList: {}
   },
   mutations: {
     getMyID (state, userId) {
