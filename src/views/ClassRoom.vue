1d73d8569a1f7364f56fbce2d9bc89e50e015a4b
kimjngyun <sanbada79@gist.ac.kr>
2020-11-07 02:56:07 +0900

    CSS grid for classroom

--- src/views/ClassRoom.vue
+++ src/views/ClassRoom.vue
@@ -1,32 +1,31 @@
 <template>
-  <v-container id="classroom">
-    <v-row>
-      <v-col cols="8">
-        <LectureView/>
-      </v-col>
-      <v-col cols="4">
-        <v-row style="height: 50%; overflow: hidden;">
-          <Video :mediaStream="hostStream" :toReverse="true"/>
-        </v-row>
-        <v-row style="height: 50%">
-          <!-- <ChatRoom/> -->
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row>
+  <div class="container">
+    <div class="item i1">
+      <!-- <Video :mediaStream="hostStream" :toReverse="true"/> -->
+      <h1>TeacherView</h1>
+    </div>
+    <div class="item i2">
+      <!-- <LectureView/> -->
+      <h1>LectureView</h1>
+    </div>
+    <div class="item i3">
+      <h1>My Avatar</h1>
+    </div>
+    <div class="item i4">
+      <h1>Seats</h1>
       <Seats/>
-    </v-row>
-  </v-container>
-</template>
+    </div>
+  </div>
 
+</template>
 <script>
 import { hostVideoMixin } from '@/mixins/hostVideoMixin.js'
 // import { expressionMixin } from '@/mixins/expressionMixin.js'
 // import ChatRoom from '@/components/ChatRoom'
-import Seats from '@/components/Seats'
+// import Seats from '@/components/Seats'
 import { classRoomMixin } from '@/mixins/classRoomMixin.js'
-import Video from '@/components/Video'
-import LectureView from '@/components/ClassRoom/LectureView'
+// import Video from '@/components/Video'
+// import LectureView from '@/components/ClassRoom/LectureView'
 // import WebSocket from 'ws'
 
 export default {
@@ -34,9 +33,9 @@ export default {
   mixins: [hostVideoMixin, classRoomMixin],
   components: {
     // ChatRoom,
-    Seats,
-    Video,
-    LectureView
+    // Seats,
+    // Video,
+    // LectureView
   },
   computed: {
   }
@@ -44,16 +43,33 @@ export default {
 </script>
 
 <style scoped>
-#classroom {
-  max-width: 100%;
-}
 
-video {
+.container {
+  max-width: 100%;
+  max-height: 100%;
+  margin: 0 auto;
+  padding: 0 auto;
+  display: grid;
   width: 100%;
   height: 100%;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: repeat(3, 33%);
+  grid-auto-flow: row;
 }
-
-img {
-  width: 100%;
+.item:nth-child(4) {
+  grid-row: span 2;
+  grid-column: span 3;
+}
+.i1 {
+  background: orange;
+}
+.i2 {
+  background: tomato;
+}
+.i3 {
+  background: burlywood;
+}
+.i4 {
+  background: blue;
 }
 </style>
